300a4ff91650740cf41e76aec28694f48f15f823
rajathshttgr <rajathshettigar.uy@gmail.com>
2026-04-18 23:04:20 +0530

    fix: remove redudent collection status check

--- zoro_client/model.py
+++ zoro_client/model.py
@@ -2,12 +2,12 @@ from enum import Enum
 
 
 class Distance(str, Enum):
-    DOT = "Dot"
-    COSINE = "Cosine"
-    L2 = "L2"
+    DOT = "dot"
+    COSINE = "cosine"
+    L2 = "l2"
 
 
 class VectorConfig:
-    def __init__(self, size=10, distance=""):
+    def __init__(self, size=1, distance=""):
         self.dimension = size
         self.distance = distance
